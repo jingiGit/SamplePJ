399a380164b5b2dd22ba72ac1c972bffe140f64e
jingiGit <job.jingi@gmail.com>
2022-04-02 12:44:25 +0900

    サインイン時にスピナーを表示する処理を追加

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../material/material.module';
 import { ErrorMessagingComponent } from './components/error-messaging/error-messaging.component';
 import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
-  declarations: [ LoadingComponent, ErrorMessagingComponent ],
-  imports: [ CommonModule ],
-  exports: [ LoadingComponent, ErrorMessagingComponent ]
+  declarations: [LoadingComponent, ErrorMessagingComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [LoadingComponent, ErrorMessagingComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/pages/components/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/components/sign-in-page/sign-in-page.component.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs';
+import { LoadingService } from 'src/app/core/services/loading.service';
 import { RoutingService } from 'src/app/core/services/routing.service';
 
 import { Component, OnInit } from '@angular/core';
@@ -28,6 +29,7 @@ export class SignInPageComponent implements OnInit {
   constructor(
     private accountService: AccountService,
     private formBuilder: FormBuilder,
+    private loadingService: LoadingService,
     private routingService: RoutingService,
     public translateService: TranslateService
   ) {}
@@ -77,6 +79,8 @@ export class SignInPageComponent implements OnInit {
   }
 
   private signIn(signInRequestDto: SignInRequestDto) {
+    // Starts Loading.
+    this.loadingService.startLoading();
     // Signs in and gets response dto.
     const signInResponseDto: Observable<SignInResponseDto> =
       this.accountService.signIn(signInRequestDto);
@@ -87,6 +91,8 @@ export class SignInPageComponent implements OnInit {
         // Moves to the Product listing page.
         this.routingService.navigate(UrlConst.PATH_PRODUCT_LISTING);
       }
+      // Stops Loading.
+      this.loadingService.stopLoading();
     });
   }
 
