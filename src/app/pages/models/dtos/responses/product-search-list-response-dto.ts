c175394e1b138b3218db01df1120d3a574e63cd3
jingiGit <job.jingi@gmail.com>
2022-05-07 14:43:32 +0900

    商品検索サービスを追加

---
+++ src/app/pages/models/dtos/responses/product-search-list-response-dto.ts
@@ -0,0 +1,7 @@
+import { BaseSearchListResponseDto } from './base-search-list-response-dto';
+import { ProductSearchResponseDto } from './product-search-response-dto';
+
+export interface ProductSearchListResponseDto
+  extends BaseSearchListResponseDto {
+  productSearchResponseDtos: ProductSearchResponseDto[];
+}
