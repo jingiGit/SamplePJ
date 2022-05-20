d4550bfd69c2a9fcd9300e6225defdac56ae386e
jingiGit <job.jingi@gmail.com>
2022-05-20 22:05:32 +0900

    ページネーターを多言語化

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 import { CoreModule } from '../core/core.module';
+import { MatPaginatorI18nService } from '../core/services/mat-paginator-i18n.service';
 import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
 import {
@@ -39,6 +41,7 @@ import {
     ReactiveFormsModule,
     NgxUpperCaseDirectiveModule,
   ],
+  providers: [{ provide: MatPaginatorIntl, useClass: MatPaginatorI18nService }],
   exports: [
     SignInPageComponent,
     ProductListingPageComponent,
