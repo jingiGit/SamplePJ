7fd29c7869d1c27174783ee24f90956cee39661e
jingiGit <job.jingi@gmail.com>
2022-06-19 14:00:04 +0900

    ProductPurchaseHistorySearchListResponseDto作成

---
+++ src/app/pages/models/dtos/responses/product-purchase-history-search-list-response-dto.ts
@@ -0,0 +1,9 @@
+import { BaseSearchListResponseDto } from './base-search-list-response-dto';
+import {
+    ProductPurchaseHistorySearchResponseDto
+} from './product-purchase-history-search-response-dto';
+
+export interface ProductPurchaseHistorySearchListResponseDto
+  extends BaseSearchListResponseDto {
+  productPurchaseHistorySearchResponseDtos: ProductPurchaseHistorySearchResponseDto[];
+}
